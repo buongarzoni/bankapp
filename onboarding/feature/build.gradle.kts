371a47a5e382bcf3471ccc09821eaf5890f32481
chris <christian.buongarzoni@gmail.com>
2023-10-26 16:57:15 -0300

    build(onboarding-feature): Add Core dependency

--- onboarding/feature/build.gradle.kts
+++ onboarding/feature/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 dependencies {
     implementation(project(":feature:components"))
     implementation(project(":onboarding:model"))
+    implementation(project(":core"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
