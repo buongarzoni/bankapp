05fe359a62e5ac6968fb55c12250942505ab006b
chris <christian.buongarzoni@gmail.com>
2023-10-30 19:06:58 -0300

    build(infrastructure-database-firebase): add home model dependency

--- infrastructure/database/firebase/build.gradle.kts
+++ infrastructure/database/firebase/build.gradle.kts
@@ -24,6 +24,7 @@ android {
 dependencies {
     implementation(project(":core"))
     implementation(project(":onboarding:model"))
+    implementation(project(":home:model"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
