a1e7f2bec60f935e5e2537b196d640afbfbf94ff
chris <christian.buongarzoni@gmail.com>
2023-10-30 16:39:48 -0300

    feat(infrastructure-auth-firebase): add login implementation

--- infrastructure/auth/firebase/src/main/java/com/bankapp/infrastructure/auth/firebase/FirebaseAuth.kt
+++ infrastructure/auth/firebase/src/main/java/com/bankapp/infrastructure/auth/firebase/FirebaseAuth.kt
@@ -9,6 +9,7 @@ import com.bankapp.onboarding.domain.Password
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException
@@ -54,12 +55,31 @@ class FirebaseAuth(
         return if (error == null) Unit.success() else error!!.error()
     }
 
+    override suspend fun login(email: Email, password: Password): Either<Int, Unit> {
+        var error: Int? = null
+
+        val task = firebaseAuth.signInWithEmailAndPassword(email.value, password.value)
+            .addOnFailureListener {
+                error = when (it) {
+                    is FirebaseAuthException -> loginError
+
+                    is FirebaseNetworkException -> networkError
+
+                    else -> unknownError
+                }
+            }
+        Tasks.whenAllComplete(task).await()
+
+        return if (error == null) Unit.success() else error!!.error()
+    }
+
     private val noLoggedUserError = R.string.infrastructure_auth_firebase_error_no_logged_user
     private val invalidIdError = R.string.infrastructure_auth_firebase_error_invalid_id
     private val emailCollisionError = R.string.infrastructure_auth_firebase_error_email_collision
     private val weakPasswordError = R.string.infrastructure_auth_firebase_error_weak_password
     private val invalidCredentialsError = R.string.infrastructure_auth_firebase_error_invalid_credentials
     private val networkError = R.string.infrastructure_auth_firebase_error_network
+    private val loginError = R.string.infrastructure_auth_firebase_error_login
     private val unknownError = R.string.infrastructure_auth_firebase_error_unknown_error
 
 }
