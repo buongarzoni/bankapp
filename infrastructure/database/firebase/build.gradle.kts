ea4c7d3102918736b716f55ce6f215d5ec5daeaa
chris <christian.buongarzoni@gmail.com>
2023-10-30 03:02:10 -0300

    build(infrastructure-database-firebase): add core and onboarding model modules dependencies

--- infrastructure/database/firebase/build.gradle.kts
+++ infrastructure/database/firebase/build.gradle.kts
@@ -22,6 +22,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":core"))
+    implementation(project(":onboarding:model"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
