87ef935b4b036ffb2f879582420992e69eb422d2
chris <christian.buongarzoni@gmail.com>
2023-10-31 01:06:24 -0300

    feat(home-model): add Empty BankDetails

--- home/model/src/main/java/com/bankapp/home/model/domain/BankDetails.kt
+++ home/model/src/main/java/com/bankapp/home/model/domain/BankDetails.kt
@@ -3,4 +3,11 @@ package com.bankapp.home.model.domain
 data class BankDetails(
     val accountBalance: AccountBalance,
     val movements: List<Movement>,
-)
+) {
+    companion object {
+        val Empty = BankDetails(
+            accountBalance = AccountBalance(Amount.Zero, Amount.Zero),
+            movements = emptyList(),
+        )
+    }
+}
