afee87d2bc65b3e44c08aa0847caf58c97cae262
chris <christian.buongarzoni@gmail.com>
2023-10-31 22:51:56 -0300

    feat(core): add map and fold for Either

--- core/src/main/java/com/bankapp/core/domain/Either.kt
+++ core/src/main/java/com/bankapp/core/domain/Either.kt
@@ -3,6 +3,20 @@ package com.bankapp.core.domain
 sealed interface Either<L, R> {
     class Error<L, R>(val error: L) : Either<L, R>
     class Success<L, R>(val value: R) : Either<L, R>
+
+    suspend fun <T> fold(fnL: (L) -> T, fnR: suspend (R) -> T): T {
+        return when (this) {
+            is Error -> fnL(error)
+            is Success -> fnR(value)
+        }
+    }
+
+    suspend fun <R2> map(fn: suspend (R) -> Either<L, R2>): Either<L, R2> {
+        return when (this) {
+            is Error -> Error(this.error)
+            is Success -> fn(value)
+        }
+    }
 }
 
 fun <L, R> R.success() = Either.Success<L, R>(this)
